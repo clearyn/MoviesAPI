bc9e328d3a8582095f5adf2695441ba908d009df
Ryan Cleary <49626962+clearyn@users.noreply.github.com>
2021-11-17 14:35:09 +0700

    Online database

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ connex_app = connexion.App(__name__, specification_dir=basedir)
 # Get the underlying Flask app instance & Configure SQLAlchemy
 app = connex_app.app
 app.config['SQLALCHEMY_ECHO'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'final_proj.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://nkyiumlhkukrjg:98422bf9d123a9160d85c595e2f4d9d10799b51468ec066512d6a9ec1488b070@ec2-52-204-72-14.compute-1.amazonaws.com:5432/d259va9fbi5tjv'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Create the SQLAlchemy db instance
